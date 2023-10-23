62572185a6c7d11bedfa57dfe2f7784d83e404dc
Junwoo-Seo <junwoo.seo.1998@gmail.com>
2023-10-23 00:23:04 -0700

    fix bug that the program cannot detect base dir

--- JEngine/JEngine/src/main.cpp
+++ JEngine/JEngine/src/main.cpp
@@ -1,8 +1,9 @@
 #include "Application.h"
 #include "Window/GLFW.h"
-
+#include <filesystem>
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** args)
 {
+	std::filesystem::current_path(std::filesystem::path(args[0]).parent_path());
 	ApplicationBuilder builder;
 	builder.SetWindow<GLFW>();
 	auto app = Application::CreateInstance(builder);
